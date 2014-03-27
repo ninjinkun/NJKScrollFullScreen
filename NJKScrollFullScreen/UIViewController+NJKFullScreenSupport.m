deaeee4e8c1cd56b00b548ca6dbf8f7f42d93ea0
ninjinkun <ninjin@mac.com>
2014-03-27 12:17:26 +0900

    Fixed broken UINavigationBar tintColor

--- NJKScrollFullScreen/UIViewController+NJKFullScreenSupport.m
+++ NJKScrollFullScreen/UIViewController+NJKFullScreenSupport.m
@@ -64,8 +64,7 @@
             // fade bar buttons
             UIColor *tintColor = self.navigationController.navigationBar.tintColor;
             if (tintColor) {
-                CGFloat *components = (CGFloat *)CGColorGetComponents(tintColor.CGColor);
-                self.navigationController.navigationBar.tintColor = [UIColor colorWithRed:components[0] green:components[1] blue:components[2] alpha:alpha];
+                self.navigationController.navigationBar.tintColor = [tintColor colorWithAlphaComponent:alpha];
             }
         }
     }];
