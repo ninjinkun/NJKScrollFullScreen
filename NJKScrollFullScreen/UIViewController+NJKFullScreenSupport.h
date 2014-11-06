e5c9b05a1be5911d6807f9e53bdc3f91feb02f73
ninjinkun <ninjin@mac.com>
2014-11-06 11:36:48 +0900

    Add compatible method for typo fixed (https://github.com/ninjinkun/NJKScrollFullScreen/pull/23)

--- NJKScrollFullScreen/UIViewController+NJKFullScreenSupport.h
+++ NJKScrollFullScreen/UIViewController+NJKFullScreenSupport.h
@@ -24,3 +24,8 @@
 - (void)setTabBarOriginY:(CGFloat)y animated:(BOOL)animated;
 
 @end
+
+// Compatible method for typo fixed (https://github.com/ninjinkun/NJKScrollFullScreen/pull/23)
+@interface UIViewController (NJKFullScreenSupportDeprecated)
+- (void)moveNavigtionBar:(CGFloat)deltaY animated:(BOOL)animated __deprecated_msg("Use `moveNavigationBar:animated:`");
+@end
